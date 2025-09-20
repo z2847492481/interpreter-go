1b9d6709811457f89efea1d4d6649874b36141f5
zhqlxl <z465719151@outlook.com>
2025-09-20 10:58:43 +0800

    1.4 扩展词法单元，支持双字符词法单元

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -20,6 +20,16 @@ func (l *Lexer) readChar() {
 	l.readPosition += 1
 }
 
+// 以不移动readPosition的方式看下一个字符是什么
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		// readPosition的位置始终指向下一个要读的字符
+		return l.input[l.readPosition]
+	}
+}
+
 // 每次阅读一个token
 // 一个token可能是变量名、运算符等
 func (l *Lexer) NextToken() token.Token {
@@ -28,7 +38,37 @@ func (l *Lexer) NextToken() token.Token {
 	l.skipWhitespace()
 	switch l.ch {
 	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
+		// 在这里判断是否是==运算符
+		if l.peekChar() == '=' {
+			// 保留下当前遍历的字符
+			ch := l.ch
+			// 再读一个字符
+			l.readChar()
+			// 拼接字面量
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.EQ, Literal: literal}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
+	case '-':
+		tok = newToken(token.MINUS, l.ch)
+	case '!':
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.NOT_EQ, Literal: literal}
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
+	case '/':
+		tok = newToken(token.SLASH, l.ch)
+	case '*':
+		tok = newToken(token.ASTERISK, l.ch)
+	case '<':
+		tok = newToken(token.LT, l.ch)
+	case '>':
+		tok = newToken(token.GT, l.ch)
 	case ';':
 		tok = newToken(token.SEMICOLON, l.ch)
 	case '(':
